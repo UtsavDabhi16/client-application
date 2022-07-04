7c20ed672cf6042d5e9e3a146d530908b8de28e0
UtsavDabhi16 <utsav.inexture@gmail.com>
2022-07-04 10:24:59 +0530

    client-application

---
+++ src/main/java/com/inexture/clientapplication/ClientApplication.java
@@ -0,0 +1,14 @@
+package com.inexture.clientapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+
+}
